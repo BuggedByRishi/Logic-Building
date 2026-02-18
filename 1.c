bf678c4725d415b5d456b1fa1331c10389e56fad
BuggedByRishi <hrushikeshkakulte@gmail.com>
2026-02-18 15:52:14 +0530

    Implementing Functions and auto Keyword to calculate the area of a circle

---
+++ 1.c
@@ -0,0 +1,28 @@
+// Area of a circle using function
+
+#include<stdio.h>
+
+float calculateArea(float fValue)
+{
+    auto const float PI = 3.14;
+    auto float ans = 0.0f;
+
+    ans = PI * fValue * fValue;
+
+    return ans;
+}
+
+int main()
+{
+    auto float fArea = 0.0f;
+    auto float fRadius = 0.0f;
+
+    printf("Enter the Radius of the Circle : ");
+    scanf("%f",&fRadius);
+
+    fArea = calculateArea(fRadius);
+
+    printf("Area of Circle is : %f\n",fArea);
+
+    return 0;
+}
